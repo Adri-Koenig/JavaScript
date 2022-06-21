d265d449565c49209b7465cf496502c29b033264
Adrián Emanuel Koenig <adrian.95.koenig@gmail.com>
2022-06-21 14:40:31 -0300

    Incorporando DOM

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,141 +1,141 @@
 /* -------------------------------------------------------------------------- */
 /*                                 Bienvenida                                 */
 /* -------------------------------------------------------------------------- */
-function nombreUsuario() {
-    let usuario = prompt("Ingrese su usuario");
-    alert("Bienvenido " + usuario);
-}
+// function nombreUsuario() {
+//     let usuario = prompt("Ingrese su usuario");
+//     alert("Bienvenido " + usuario);
+// }
 
-nombreUsuario();
+// nombreUsuario();
 
 /* -------------------------------------------------------------------------- */
 /*                                  Datos                                     */
 /* -------------------------------------------------------------------------- */
 
-const games = ["1. League of Legends", "2. Dota 2", "3. Valorant", "4 .Counter Strike", "5. Fortnite", "6. Apex Legends", "7. PUBG"];
+// const games = ["1. League of Legends", "2. Dota 2", "3. Valorant", "4 .Counter Strike", "5. Fortnite", "6. Apex Legends", "7. PUBG"];
 
-games.pop(6);
+// games.pop(6);
 
-function chooseRank() {
-    let opcion = Number(prompt("Seleccione un juego " + '\n' + "Elija una opcion (Colocar el número correspondiente):" + "\n" + games.join("\n")))
+// function chooseRank() {
+//     let opcion = Number(prompt("Seleccione un juego " + '\n' + "Elija una opcion (Colocar el número correspondiente):" + "\n" + games.join("\n")))
 
-    while (true) {
-        if (opcion == 1) {
-            let rank = [
-                "1. Hierro",
-                "2. Bronce",
-                "3. Plata",
-                "4. Oro",
-                "5. Platino",
-                "6. Diamante",
-                "7. Master",
-                "8. GrandMaster",
-            ]
+//     while (true) {
+//         if (opcion == 1) {
+//             let rank = [
+//                 "1. Hierro",
+//                 "2. Bronce",
+//                 "3. Plata",
+//                 "4. Oro",
+//                 "5. Platino",
+//                 "6. Diamante",
+//                 "7. Master",
+//                 "8. GrandMaster",
+//             ]
 
-            rank.push("9. Challenger");
+//             rank.push("9. Challenger");
 
-            let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
-            break
-        }
+//             let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
+//             break
+//         }
 
-        if (opcion == 2) {
-            let rank = [
-                "1. Heraldo",
-                "2. Guardián",
-                "3. Cruzado",
-                "4. Arconte",
-                "5. Leyenda",
-                "6. Ancestral",
-                "7. Divino",
-                "8. Inmortal",
-            ]
-            let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
-            break
-        }
+//         if (opcion == 2) {
+//             let rank = [
+//                 "1. Heraldo",
+//                 "2. Guardián",
+//                 "3. Cruzado",
+//                 "4. Arconte",
+//                 "5. Leyenda",
+//                 "6. Ancestral",
+//                 "7. Divino",
+//                 "8. Inmortal",
+//             ]
+//             let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
+//             break
+//         }
 
-        if (opcion == 3) {
-            let rank = [
-                "1. Hierro",
-                "2. Bronce",
-                "3. Plata",
-                "4. Oro",
-                "5. Platino",
-                "6. Diamante",
-                "7. Inmortal",
-                "8. Radiante",
-            ]
-            let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
-            break
-        }
+//         if (opcion == 3) {
+//             let rank = [
+//                 "1. Hierro",
+//                 "2. Bronce",
+//                 "3. Plata",
+//                 "4. Oro",
+//                 "5. Platino",
+//                 "6. Diamante",
+//                 "7. Inmortal",
+//                 "8. Radiante",
+//             ]
+//             let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
+//             break
+//         }
 
-        if (opcion == 4) {
-            let rank = [
-                "1. Silver",
-                "2. Gold Nova",
-                "3. Master Guardian",
-                "4. Master Guardian Elite",
-                "5. Distinguished Master Guardian",
-                "6. Legendary Eagle",
-                "7. Legendary Eagle Master",
-                "8. Supreme Master First Class",
-                "9. The Global Elite",
-            ]
-            let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
-            break
-        }
+//         if (opcion == 4) {
+//             let rank = [
+//                 "1. Silver",
+//                 "2. Gold Nova",
+//                 "3. Master Guardian",
+//                 "4. Master Guardian Elite",
+//                 "5. Distinguished Master Guardian",
+//                 "6. Legendary Eagle",
+//                 "7. Legendary Eagle Master",
+//                 "8. Supreme Master First Class",
+//                 "9. The Global Elite",
+//             ]
+//             let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
+//             break
+//         }
 
-        if (opcion == 5) {
-            let rank = [
-                "1. Bronze",
-                "2. Silver",
-                "3. Gold",
-                "4. Platinum",
-                "5. Champion",
-            ]
-            let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
-            break
-        }
+//         if (opcion == 5) {
+//             let rank = [
+//                 "1. Bronze",
+//                 "2. Silver",
+//                 "3. Gold",
+//                 "4. Platinum",
+//                 "5. Champion",
+//             ]
+//             let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
+//             break
+//         }
 
-        if (opcion == 6) {
-            let rank = [
-                "1. Bronze",
-                "2. Silver",
-                "3. Gold",
-                "4. Platinum",
-                "5. Diamond",
-                "6. Apex Predator",
-            ]
-            let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
-            break
-        } else {
-            alert("Opción no valida. Por favor seleccione la opción correcta.")
-            opcion = Number(prompt("Seleccione un juego " + '\n' + "Elija una opcion (Colocar el número correspondiente):" + "\n" + games.join("\n")))
-        }
-    }
-}
+//         if (opcion == 6) {
+//             let rank = [
+//                 "1. Bronze",
+//                 "2. Silver",
+//                 "3. Gold",
+//                 "4. Platinum",
+//                 "5. Diamond",
+//                 "6. Apex Predator",
+//             ]
+//             let ranking = Number(prompt("Seleccione su Rango " + "\n" + rank.join("\n")))
+//             break
+//         } else {
+//             alert("Opción no valida. Por favor seleccione la opción correcta.")
+//             opcion = Number(prompt("Seleccione un juego " + '\n' + "Elija una opcion (Colocar el número correspondiente):" + "\n" + games.join("\n")))
+//         }
+//     }
+// }
 
 /* -------------------------------------------------------------------------- */
 /*                                  Promedio                                  */
 /* -------------------------------------------------------------------------- */
 
-function promedio() {
-    let partidas = parseInt(prompt("Número de partidas jugadas en el día."));
-    let total = 0;
-    for (let i = 1; i <= partidas; i++) {
-        total = total + parseInt(prompt("¿Cuántos asesinatos realizó?"));
-        console.log(total);
-    }
+// function promedio() {
+//     let partidas = parseInt(prompt("Número de partidas jugadas en el día."));
+//     let total = 0;
+//     for (let i = 1; i <= partidas; i++) {
+//         total = total + parseInt(prompt("¿Cuántos asesinatos realizó?"));
+//         console.log(total);
+//     }
 
-    let promedio = total / partidas;
-    if (promedio <= 8) {
-        alert("Tienes que mejorar. Su promedio de asesinatos es de " + promedio + ".");
-    } else {
-        alert("Felicidades. Su promedio de asesinatos es de " + promedio + ".");
-    }
-}
+//     let promedio = total / partidas;
+//     if (promedio <= 8) {
+//         alert("Tienes que mejorar. Su promedio de asesinatos es de " + promedio + ".");
+//     } else {
+//         alert("Felicidades. Su promedio de asesinatos es de " + promedio + ".");
+//     }
+// }
 
-chooseRank();
-promedio();
+// chooseRank();
+// promedio();
 
 /* -------------------------------------------------------------------------- */
 /*                                  Usuarios                                  */
@@ -155,7 +155,7 @@ const users = [{
     User: "xBlake",
     Game: "League of Legends",
     Rank: "Gold I",
-    Played: 60,
+    Played: 36,
     Wins: 26,
     Losses: 10,
     Winrate: 66.7
@@ -186,7 +186,7 @@ const users = [{
     Wins: 36,
     Losses: 15,
     Winrate: 70.5
-}];
+},];
 
 /* -------------------------------------------------------------------------- */
 /*                             Busqueda de Usuario                            */
@@ -227,8 +227,8 @@ let rank = [
 
 const Id1 = users.length + 1
 const User1 = prompt("Ingresar su usuario")
-const Game1 = prompt("Seleccionar Juego"+ "\n"+ games2.join("\n"))
-const Rank1 = prompt("Seleccionar Rango" +"\n" + rank.join("\n"))
+const Game1 = prompt("Seleccionar Juego" + "\n" + games2.join("\n"))
+const Rank1 = prompt("Seleccionar Rango" + "\n" + rank.join("\n"))
 const Played1 = Number(prompt("Partidas Jugadas"))
 const Wins1 = Number(prompt("Partidas Ganadas"))
 const Losses1 = Played1 - Wins1
@@ -246,4 +246,16 @@ users.push({
 })
 
 console.log(users);
-alert(User1 + "\n" + Game1 + "\n" + Rank1+ "\n" + "\n" + "Winrate: " + Winrate1 + "%")
+let profiles = document.getElementById("profiles");
+
+for (const data of users) {
+    let card = document.createElement("div");
+    card.innerHTML =
+        `<h3> ${data.User}</h3>
+        <h4> ${data.Game}</h4>
+        <p> ${data.Rank} </p>
+        <p> Wins: ${data.Wins} - Losses: ${data.Losses}</p>
+        <p><b> Winrate: ${data.Winrate}</b></p>`;
+    profiles.appendChild(card);
+    card.className = "perfil"
+}
